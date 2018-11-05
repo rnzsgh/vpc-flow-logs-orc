8626de2bce1c96dd7286083189c56ffaef2d0d3d
Ryan <ryannitz@amazon.com>
2018-11-05 09:41:01 -0500

    Simple

--- src/main/java/demo/VpcFlowLogsToOrc.java
+++ src/main/java/demo/VpcFlowLogsToOrc.java
@@ -1,25 +1,6 @@
 /**
  * Any code, applications, scripts, templates, proofs of concept,
  * documentation and other items are provided for illustration purposes only.
- *
- * Copyright 2017 Ryan Nitz
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- * Bastion stack creation prerequisite:  first create an EC2 key pair and a VPC stack.
- * For details about how to connect to a Linux instance in a private subnet via the
- * bastion, see the following AWS blog post:
- * https://aws.amazon.com/blogs/security/securely-connect-to-linux-instances-running-in-a-private-amazon-vpc/
  */
 
 package demo;
